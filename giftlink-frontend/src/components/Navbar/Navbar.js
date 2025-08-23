6d5f3b3f914b3ac27db8e9ea409c0243b842b42f
segmur <shegun524@gmail.com>
2025-08-23 06:44:45 -0400

    modified Navbar.js file

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -10,14 +10,14 @@ export default function Navbar() {
                     {/* Task 1: Add links to Home and Gifts below*/}
                     
                     <li className="nav-item">
-                        
+
                     <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/app/search">Search</Link>
+                        <a className="nav-link" href="/app/search">Search</a>
                     </li>
                 </ul>
             </div>
