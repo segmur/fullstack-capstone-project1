5b7ddcdc277af00eea91ad42bb637ec16deaac99
segmur <shegun524@gmail.com>
2025-08-23 06:30:17 -0400

    update implementation of search page

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -8,14 +8,16 @@ export default function Navbar() {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     {/* Task 1: Add links to Home and Gifts below*/}
+                    
                     <li className="nav-item">
-                        <Link className="nav-link" to="/app/search">Search</Link>
-                    </li>
-                    <li className="nav-item">
+                        
                     <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/app/search">Search</Link>
                     </li>
                 </ul>
             </div>
