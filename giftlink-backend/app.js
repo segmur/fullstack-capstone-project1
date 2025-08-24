a93a618fdb6c4f5e2ab74efd1074e96ea218d075
segmur <shegun524@gmail.com>
2025-08-24 07:52:05 -0400

    Implemented Back end Integration for Registration

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -24,6 +24,7 @@ app.use(express.json());
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftRoutes
 const giftRoutes = require("./routes/giftRoutes");
+const authRoutes = require('./routes/authRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require("./routes/searchRoutes");
@@ -37,9 +38,11 @@ app.use(pinoHttp({ logger }));
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use('/api/gifts', giftRoutes);
+app.use('/api/auth', authRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
+
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
